d86cf04a65b41d5fc7b89f1e3f2e0bdaf00ef824
KRACHIK <bykrachik@gmail.com>
2016-02-05 18:30:01 +0300

    add 2 thread

--- source/main.cpp
+++ source/main.cpp
@@ -9,69 +9,109 @@
 #include <boost/asio.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/enable_shared_from_this.hpp>
+
+#include <string>
+
+
 using namespace boost::asio;
 using boost::system::error_code;
 io_service service;
 
+
 ip::tcp::endpoint ep(ip::address::from_string("127.0.0.1"), 8001);
 
-size_t read_complete(char * buf, const error_code & err, size_t bytes) {
-	if (err) return 0;
-	bool found = std::find(buf, buf + bytes, '\n') < buf + bytes;
-	// we read one-by-one until we get to enter, no buffering
-	return found ? 0 : 1;
+
+
+struct talk_to_svr
+{
+	talk_to_svr(const std::string & username) : sock_(service), started_(true), username_(username)
+	{	}
+
+
+	void connect(ip::tcp::endpoint ep) {
+		sock_.connect(ep);
+	}
+
+	void messages(const std::string & msg){
+		write(msg);
+	}
+
+	void GetDataArival(){
+		read_answer();
+	}
+	std::string username() const { return username_; }
+
+private:
+	size_t read_complete(const boost::system::error_code & err, size_t bytes) {
+		if (err) return 0;
+		already_read_ = bytes;
+		bool found = std::find(buff_, buff_ + bytes, '\n') < buff_ + bytes;
+		return found ? 0 : 1;// we read one-by-one until we get to enter, no buffering
+	}
+
+	void write(const std::string & msg) {
+		sock_.write_some(buffer(msg));
+	}
+
+	void read_answer() {
+		already_read_ = 0;
+		read(sock_, buffer(buff_),
+			boost::bind(&talk_to_svr::read_complete, this, _1, _2));
+		std::string msg(buff_, already_read_);
+		std::cerr << "<--\t" << msg << std::endl;
+	}
+private:
+	ip::tcp::socket sock_;
+	enum { max_msg = 1024 };
+	int already_read_;
+	char buff_[max_msg];
+	bool started_;
+	std::string username_;
+};
+
+talk_to_svr client("del prms");
+
+void run_client()
+{
+	const int MAX = 1023;
+	char msg[MAX] = "";
+	std::string str;
+	while (true)
+	{
+		std::cout << "->";
+		//std::cin.getline(msg, MAX);
+		std::cin >> str;
+		str += '\n';
+		client.messages(str);
+		boost::this_thread::sleep(boost::posix_time::millisec(111));
+	}
+}
+void Rider()
+{
+	while (true){
+		client.GetDataArival();
+		boost::this_thread::sleep(boost::posix_time::millisec(1));
+	}
 }
 
-void sync_echo() {
 
-
-
-
-	std::string msg;
-	std::cin >> msg;
-	msg += "\n";
-
-	ip::tcp::socket sock(service);
-	sock.connect(ep);
-	sock.write_some(buffer(msg));
-	char buf[1024];
-	int bytes = read(sock, buffer(buf), boost::bind(read_complete, buf, _1, _2));
-	std::string copy(buf, bytes - 1);
-	msg = msg.substr(0, msg.size() - 1);
-	std::cout << "->" << msg << ": " << (copy == msg ? "OK" : "FAIL") << std::endl;
-	
-	 sock.close(); 
-
-}
-
-int main(int argc, char* argv[]) {
-	std::cout << "write message\n";	
-	
-	
-	// connect several clients
-	/*char* messages[] = { "John says hi", "so does James",
-		"Lucy just got home", "Boost.Asio is Fun!", 0 };
-	boost::thread_group threads;
-	for (char ** message = messages; *message; ++message) {
-		threads.create_thread(boost::bind(sync_echo, *message));
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "Russian");
+	try
+	{
+		client.connect(ep);
+		Sleep(333);
+		boost::thread_group threads;
+		threads.create_thread(boost::bind(run_client));
+		threads.create_thread(boost::bind(Rider));
 		boost::this_thread::sleep(boost::posix_time::millisec(100));
+		threads.join_all();
 	}
-	threads.join_all();*/
-
-	
-	//sock.connect(ep);
-
-
-
-	// tut dolzhen by`t` potok priniatiia vhodiashchikh
-
-	while (true){ // zamenit` na odin potok 
-		sync_echo(); //pizdetc
+	catch (boost::system::system_error & err) {
+		std::cout << "error conections\n";
 	}
-	//sock.close(); // peremestit` v soby`tie zakry`tiia prilozheniia
-	system("pause");
 }
-
 
 
 
